9066add75f3ba6fe17147cbfef4ba02d912c22e2
ck <btd9462ptp@privaterelay.appleid.com>
2026-06-23 23:19:27 +0200

    feat: add constants

---
+++ custom_components/domuse_irrigation/const.py
@@ -0,0 +1,10 @@
+"""Constants for Domuse Irrigation."""
+
+DOMAIN = "domuse_irrigation"
+STORAGE_KEY = f"{DOMAIN}_data"
+STORAGE_VERSION = 1
+CONF_SHOW_IN_SIDEBAR = "show_in_sidebar"
+
+STATIC_PATH = "/domuse_irrigation_static"
+PANEL_URL = f"{STATIC_PATH}/domuse-irrigation-panel.js"
+CARD_URL  = f"{STATIC_PATH}/domuse-irrigation-card.js"
